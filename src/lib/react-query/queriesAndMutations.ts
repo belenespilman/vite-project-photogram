fd0a877f0c3adbd3d17214b04e10406c09d7ab59
belenespilman <belen.espilman@gmail.com>
2024-03-07 17:36:34 +0100

    createPost component, textarea added

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -5,6 +5,7 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/react-query/queryKeys";
 import { createUserAccount } from "../appwrite/api";
 import { account } from "../appwrite/config";
 import { signOutAccount } from "../appwrite/api";
@@ -34,5 +35,31 @@ export async function signInAccount(user: { email: string; password: string }) {
 export const useSignoutAccount = () => {
   return useMutation({
     mutationFn: () => signOutAccount()
+  });
+};
+
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
+};
+
+
+export const useUpdatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: IUpdatePost) => updatePost(post),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      });
+    },
   });
 };
